f55fa0700775a7bde66adc95653f8a21f3bdf6cc
sa-webb <sawebb30@gmail.com>
2020-05-04 19:15:45 -0500

    test

--- index.js
+++ index.js
@@ -50,6 +50,6 @@ const server = new ApolloServer({
 });
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
 });
