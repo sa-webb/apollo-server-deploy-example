afd9a6cff5dff137c01257016f1bf04560452d6e
sa-webb <sawebb30@gmail.com>
2020-05-05 14:15:35 -0500

    refactored

--- index.js
+++ index.js
@@ -1,64 +1,9 @@
 const { ApolloServer } = require('apollo-server');
 const { config } = require('dotenv');
 const { typeDefs } = require('./typeDefs')
+const { templates } = require('./data')
 
 config();
-
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-
-const owner1 = {
-  name: 'Austin',
-  email: 'austin@example.com',
-};
-
-const owner2 = {
-  name: 'Drew',
-  email: 'drew@example.com',
-};
-
-const meta1 = {
-  id: 1,
-  owner: owner1,
-  industry: 'Software Developer',
-};
-
-const meta2 = {
-  id: 1,
-  owner: owner1,
-  industry: 'Sales Associate',
-};
-
-const education1 = [
-  {
-    university: 'University of Tennessee',
-    degree_type: 'Bachelor of Science',
-  },
-  { university: 'Harvard University', degree_type: 'MBA' }
-];
-
-const education2 = [{
-  university: 'Michigan University',
-  degree_type: 'MBA',
-}];
-
-const templates = [
-  {
-    id: 1,
-    type: 'template1',
-    owner: owner1,
-    meta: meta1,
-    educations: education1,
-  },
-  {
-    id: 2,
-    type: 'template2',
-    owner: owner2,
-    meta: meta2,
-    educations: education2
-  },
-];
 
 const resolvers = {
   Query: {
@@ -66,11 +11,17 @@ const resolvers = {
   },
 };
 
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   playground: true,
   introspection: true,
+  engine: {
+    apiKey: process.env.ENGINE_API_KEY,
+  },
 });
 
 // The `listen` method launches a web server.
